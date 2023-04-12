acdbe03c0d4c18db063fbc3052e6666a1d041263
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-04-12 09:26:30 +0000

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""MyInt class module"""
+"""Module containing add_attribute method"""
 
 
-class MyInt(int):
-    """A MyInt class"""
-    def __eq__(self, other):
-        """Overides and inverts == operator"""
-        return int(self) != int(other)
-
-    def __ne__(self, other):
-        """Overides and inverts != operator"""
-        return int(self) == int(other)
+def add_attribute(obj, name, value):
+    """Method checking if attribute can be set and sets
+    where possible"""
+    if hasattr(obj, "__dict__") or \
+       (hasattr(obj, "__slots__") and name in obj.__slots__):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
