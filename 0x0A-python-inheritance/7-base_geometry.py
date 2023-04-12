a3cdcc679a8d260971537b45daeca32f05565365
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-04-12 09:29:57 +0000

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,7 +5,7 @@
 class BaseGeometry():
     """BaseGeometry class"""
     def area(self):
-        """method for calculating area"""
+        """Method Raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
