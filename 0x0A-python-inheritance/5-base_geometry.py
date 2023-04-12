369432c88740fb224a75dc87d4705d4c9aa5a8db
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-04-12 08:42:15 +0000

    Update 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
+
 class BaseGeometry:
     """ Empty class """
     pass
