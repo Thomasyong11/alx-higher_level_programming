38603831fdeedb4c9feff653e06ad8de9b855948
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-04-12 13:06:17 +0000

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """function that reads a text file and prints it"""
+""" Module that contains a function that reads from a file """
 
-    with open(filename) as f:
-        text = f.read()
-        print(text, end="")
+
+def read_file(filename=""):
+    """ Function that reads from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
