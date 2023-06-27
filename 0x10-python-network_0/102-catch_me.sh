161a8b69910c0496b134c243da4560da1403986d
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-06-27 23:59:36 +0000

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:You got me!"
+response=$(curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:You got me!")
+
+# Store the message in a variable
+message=$(grep -o "You got me!" <<< "$response")
+
+# Print the message by redirecting it to stdout
+printf "%s\n" "$message"
