80708b9290b5239ea047610b8f08cc93bc4543ad
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-06-27 23:55:33 +0000

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -s 0.0.0.0:5000/catch_me -XPUT -d "user_id=98" -L -o response.txt
 
-# Send a POST request to the specified URL with a custom header
-curl -s -X POST -H "Origin: HolbertonSchool" -d "user_id=98" "http://0.0.0.0:5000/catch_me"
+# Print the message from the response file
+grep -o "You got me!" response.txt
